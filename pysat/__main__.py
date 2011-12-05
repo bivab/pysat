195d94ebea0c49e6499d1d620d48022a40288f25
David Schneider <david.schneider@picle.org>
2011-12-05 16:04:09 +0100

    entry point

--- pysat/__main__.py
+++ pysat/__main__.py
@@ -2,14 +2,21 @@ import sys, os
 from pysat.parser import CNFParser
 
 def main(argv):
+    """Main entry point of the stand-alone executable:
+    takes a file and invokes the solver on it
+    """
+    if len(argv) < 2:
+        print "Usage: %s filename" % (argv[0],)
+        return 2
     f = argv[1]
     assert os.path.isfile(f)
     formula = CNFParser().parse_file(f)
     assignment = formula.dpll()
-    #print repr(formula)
     if assignment:
         print 'SATISFIABLE'
     else:
         print 'NOT SATISFIABLE'
+    return 0
 
-main(sys.argv)
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
